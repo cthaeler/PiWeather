81bb12db43a6f25940fa5e264408bc37a6dee895
Charles Thaeler <cst@soar-high.com>
2017-02-03 14:59:01 -0800

    Improved the build script

--- build.sh
+++ build.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
+rm *.class *.jar
 javac DataValue.java
 javac ForecastDataValue.java
 javac MapUpdateTimer.java
 javac TimeUpdateTimer.java
 javac UpdateUITimer.java
+javac UpdateSensorTimer.java
 javac PiWeather.java
+#jar cmvf MANIFEST.MF PiWeather.jar *.class *.MF
