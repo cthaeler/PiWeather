7fed8659aeb447c8ac2389813b381e5ffab7fbef
Charles Thaeler <cst@soar-high.com>
2017-02-21 16:47:14 -0800

    Added the new sensor modules to the build script

--- build.sh
+++ build.sh
@@ -8,5 +8,11 @@ javac TimeUpdateTimer.java
 javac UIUpdateTimer.java
 javac SensorUpdateTimer.java
 javac TrendDisplayPanel.java
+javac WxSensor.java
+javac BME280_Sensor.java
+javac BMP280_Sensor.java
+javac DHT11_Sensor.java
+javac DHT22_Sensor.java
+javac Dummy_Sensor.java
 javac PiWeather.java
 jar cfm PiWeather.jar MANIFEST.MF *.class
